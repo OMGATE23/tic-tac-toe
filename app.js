cacc7933f017664ccb8aab170abe9108e4c8f70b
omgate <90276220+OMGATE23@users.noreply.github.com>
2022-11-04 20:40:17 +0530

    feat: handle click by placing mark and swap turn

--- app.js
+++ app.js
@@ -16,3 +16,27 @@ const WINNING_COMBINATIONS = [
     [0,4,8],
     [2,4,6]
 ]
+
+startGame()
+function startGame(){
+    cellElement.forEach(cell => {
+        cell.addEventListener('click' , handleClick, {once : true})
+    })
+}
+
+function handleClick(e){
+    cell = e.target
+
+    currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+    swapTurn()
+    setBoardHoverClass()
+}
+
+function placeMark(cell, currentClass){
+    cell.classList.add(currentClass)
+}
+
+function swapTurn(){
+    circleTurn = !circleTurn
+}
